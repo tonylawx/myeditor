e7a2ef861967074431637e9b92bc8125c6268f10
tonylaw <tony.luo@delonix.group>
2022-09-24 15:07:07 +0800

    style: eslint fix

--- packages/form/src/fields/bwFields/HoverWindowConfig.vue
+++ packages/form/src/fields/bwFields/HoverWindowConfig.vue
@@ -1,60 +1,70 @@
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
 <template>
   <div>
     <el-form>
       <el-form-item label="选择图片">
-        <el-upload>
-          <el-icon  class="avatar-uploader-icon"><Plus /></el-icon>
-        </el-upload>
+        <UploadCustom
+          :showCropper="false"
+          text="上传图片"
+          :limit="1"
+          urlModel
+          v-model:url="res.bgUrl"
+        />
       </el-form-item>
       <el-form-item label="图片跳转">
-        <el-button >选择链接</el-button>
+        <LinkSelector v-model:link-option="res.link" />
       </el-form-item>
       <el-form-item label="距离底部">
-          <el-slider v-model="res.bottomDis" max="150"/>px
+        <el-slider v-model="res.bottomDis" max="150" />px
       </el-form-item>
     </el-form>
   </div>
 </template>
 <script lang="ts" setup>
-import { defineProps, reactive, ref,watch} from 'vue';
-import fieldProps from '../../utils/fieldProps';
-import { useAddField } from '../../utils/useAddField';
-import {cloneDeep} from "lodash-es";
-import {MFloatCom} from "../../../../bwComponentType";
-import { Plus } from '@element-plus/icons-vue'
+import { defineProps, reactive, ref, watch } from "vue";
+import fieldProps from "../../utils/fieldProps";
+import { useAddField } from "../../utils/useAddField";
+import { cloneDeep } from "lodash-es";
+import { MFloatCom } from "../../../../bwComponentType";
+import LinkSelector from "./components/LinkSelector.vue";
+import { UploadCustom } from "./components/upload-custom";
 
 const props = defineProps({
   ...fieldProps,
-  config:{
-    type:Object,
-    default:()=>{}
-  }
+  config: {
+    type: Object,
+    default: () => {},
+  },
 });
 
 useAddField(props.prop);
-const emit = defineEmits(['change'])
+const emit = defineEmits(["change"]);
 
-const res = reactive(Object.assign({},{
-  bgUrl: '',
-  link: {
-    type: 'MiniProgram',
-    appId: '',
-    url: ''
-  },
-  bottomDis: ''
-},cloneDeep(props.model[props.name]))) as MFloatCom
+const res = reactive(
+  Object.assign(
+    {},
+    {
+      bgUrl: "",
+      link: {
+        type: "MiniProgram",
+        appId: "",
+        url: "",
+      },
+      bottomDis: "",
+    },
+    cloneDeep(props.model[props.name])
+  )
+) as MFloatCom;
 // table实例
 // 对话框flag
 const dialogVisible = ref(false);
 watch(res, (value) => {
-  emit('change',value)
-})
+  emit("change", value);
+});
 </script>
 
 <script lang="ts">
 export default {
-  name: 'm-fields-hover-window-config',
+  name: "m-fields-hover-window-config",
 };
 </script>
--- packages/form/src/fields/bwFields/RichText.vue
+++ packages/form/src/fields/bwFields/RichText.vue
@@ -1,31 +1,40 @@
 <template>
-  <VueEditor v-model:htmlValue="res.text"/>
+  <VueEditor v-model:htmlValue="res.text" />
   <el-button @click="handleSave">保存</el-button>
 </template>
 
 <script setup lang="ts">
+import { defineProps, reactive } from 'vue';
+import { cloneDeep } from 'lodash-es';
+
+import { useAddField } from '@tmagic/form';
+
+import { MTextCom } from '../../../../bwComponentType';
+import fieldProps from '../../utils/fieldProps';
+
 import { VueEditor } from './components/vue-editor';
-import {defineProps, reactive, ref, watch} from "vue";
-import fieldProps from "../../utils/fieldProps";
-import {useAddField} from "@tmagic/form";
-import {cloneDeep} from "lodash-es";
-import {MTextCom} from "../../../../bwComponentType";
 const props = defineProps({
   ...fieldProps,
-  config:{
-    type:Object,
-    default:()=>{}
-  }
+  config: {
+    type: Object,
+    default: () => {},
+  },
 });
 
 useAddField(props.prop);
-const emit = defineEmits(['change'])
-const res = reactive(Object.assign({},{
-  text:''
-},cloneDeep(props.model[props.name]))) as MTextCom
-const handleSave=()=>{
-  emit('change',res)
-}
+const emit = defineEmits(['change']);
+const res = reactive(
+  Object.assign(
+    {},
+    {
+      text: '',
+    },
+    cloneDeep(props.model[props.name]),
+  ),
+) as MTextCom;
+const handleSave = () => {
+  emit('change', res);
+};
 </script>
 <script lang="ts">
 export default {
